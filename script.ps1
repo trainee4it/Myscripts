8db2caeb5dcdf632dcb00505e5481874252aaaf6
trainee4it <69849181+trainee4it@users.noreply.github.com>
2022-03-30 10:52:45 +0200

    create script.ps1

---
+++ script.ps1
@@ -0,0 +1,46 @@
+﻿'Naam is ' 
+
+$Name = Read-Host
+
+
+'kleur is ' 
+
+$Color = Read-Host
+
+
+'Weer is ' 
+
+
+$Weather = Read-Host
+
+
+if($Name -eq 'Julia' -and $Color -eq 'Green' -and $Weather -eq 'Mooi')
+{
+
+
+    ' welcome ' +$Name
+
+
+
+}
+
+
+elseif($Name -eq 'Dymfna' -and $Color -eq 'red' -and $Weather -eq 'Mooi')
+{
+
+
+    ' welcome ' +$Name
+
+
+
+}
+
+else
+{
+
+
+
+    ' I do not know you ' + $Name
+
+
+}
